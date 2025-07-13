e34126dad8b622f9463c2c01afc78662916b0f0c
Wafiul Achdi <achdiwafiul@gmail.com>
2025-07-13 23:55:05 +0700

    feat(dropdowns): add action dropdowns for each uploaded file

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -1,11 +1,71 @@
-import React from 'react'
+'use client';
 
-const ActionDropdown = ({}) => {
+import React, { useState } from 'react';
+
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import Image from 'next/image';
+import { Models } from 'node-appwrite';
+import { actionsDropdownItems } from '@/constants';
+import { constructDownloadUrl } from '@/lib/utils';
+import Link from 'next/link';
+
+const ActionDropdown = ({ file }: { file: Models.Document }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [action, setAction] = useState<ActionType | null>(null);
+
+  const renderDialogContent = () => {
+    return (
+      <DialogContent className='shad-dialog button'>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>This action cannot be undone. This will permanently delete your account and remove your data from our servers.</DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    );
+  };
   return (
-    <div>
-      test
-    </div>
-  )
-}
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+        <DropdownMenuTrigger className="shad-no-focus">
+          <Image src="/assets/icons/dots.svg" alt="dots" width={34} height={34} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="max-w-[200px] truncate">{file.name}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {actionsDropdownItems.map((actionItem) => (
+            <DropdownMenuItem
+              key={actionItem.value}
+              className="shad-dropdown-item"
+              onClick={() => {
+                setAction(actionItem);
 
-export default ActionDropdown
+                if (['rename', 'share', 'delete', 'details'].includes(actionItem.value)) {
+                  setIsModalOpen(true);
+                }
+              }}
+            >
+              {actionItem.value === 'download' ? (
+                <Link href={constructDownloadUrl(file.bucketFileId)} download={file.name} className="flex items-center gap-2">
+                  <Image src={actionItem.icon} alt={actionItem.label} width={30} height={30} />
+                  {actionItem.label}
+                </Link>
+              ) : (
+                <div className="flex items-center gap-2">
+                  <Image src={actionItem.icon} alt={actionItem.label} width={30} height={30} />
+                  {actionItem.label}
+                </div>
+              )}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {renderDialogContent()}
+    </Dialog>
+  );
+};
+
+export default ActionDropdown;
