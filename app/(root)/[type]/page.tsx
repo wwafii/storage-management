638e461fa0aef286f3068670236924b07c16ea45
Wafiul Achdi <achdiwafiul@gmail.com>
2025-07-14 15:18:52 +0700

    feat: add total file size display for each file type

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,17 +1,28 @@
-import Card from '@/components/Card';
-import Sort from '@/components/Sort';
-import { getFiles } from '@/lib/actions/file.actions';
-import { getFileTypesParams } from '@/lib/utils';
-import { Models } from 'node-appwrite';
-import React from 'react';
+import React from "react";
+import Sort from "@/components/Sort";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+import Card from "@/components/Card";
+import { getFileTypesParams } from "@/lib/utils";
 
 const Page = async ({ searchParams, params }: SearchParamProps) => {
-  const type = ((await params)?.type as string) || '';
-  const searchText = ((await searchParams)?.query as string) || '';
-  const sort = ((await searchParams)?.sort as string) || '';
+  const type = ((await params)?.type as string) || "";
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
 
   const types = getFileTypesParams(type) as FileType[];
+
   const files = await getFiles({ types, searchText, sort });
+
+  
+  const totalSizeBytes = files.documents.reduce(
+    (acc: number, file: Models.Document) => acc + (file.size || 0),
+    0
+  );
+
+  
+  const totalSizeMB = (totalSizeBytes / (1024 * 1024)).toFixed(2);
+
   return (
     <div className="page-container">
       <section className="w-full">
@@ -19,20 +30,22 @@ const Page = async ({ searchParams, params }: SearchParamProps) => {
 
         <div className="total-size-section">
           <p className="body-1">
-            Total: <span className="h5">0 MB</span>
+            Total: <span className="h5">{totalSizeMB} MB</span>
           </p>
 
           <div className="sort-container">
-            <p className="body-1 hidden sm:block text-light-200">Sort by:</p>
+            <p className="body-1 hidden text-light-200 sm:block">Sort by:</p>
+
             <Sort />
           </div>
         </div>
       </section>
 
+      {/* Render the files */}
       {files.total > 0 ? (
         <section className="file-list">
           {files.documents.map((file: Models.Document) => (
-            <Card key={file.$id} file={file}/>
+            <Card key={file.$id} file={file} />
           ))}
         </section>
       ) : (
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,24 +1,34 @@
-'use server';
+"use server";
 
-import { createAdminClient } from '../appwrite';
-import { InputFile } from 'node-appwrite/file';
-import { appwriteConfig } from '../appwrite/config';
-import { ID, Models, Query } from 'node-appwrite';
-import { constructFileUrl, getFileType, parseStringify } from '../utils';
-import { revalidatePath } from 'next/cache';
-import { getCurrentUser } from './user.actions';
+import { createAdminClient, createSessionClient } from "@/lib/appwrite";
+import { InputFile } from "node-appwrite/file";
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { ID, Models, Query } from "node-appwrite";
+import { constructFileUrl, getFileType, parseStringify } from "@/lib/utils";
+import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "@/lib/actions/user.actions";
+
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
   throw error;
 };
 
-export const uploadFile = async ({ file, ownerId, accountId, path }: UploadFileProps) => {
+export const uploadFile = async ({
+  file,
+  ownerId,
+  accountId,
+  path,
+}: UploadFileProps) => {
   const { storage, databases } = await createAdminClient();
 
   try {
     const inputFile = InputFile.fromBuffer(file, file.name);
 
-    const bucketFile = await storage.createFile(appwriteConfig.bucketId, ID.unique(), inputFile);
+    const bucketFile = await storage.createFile(
+      appwriteConfig.bucketId,
+      ID.unique(),
+      inputFile,
+    );
 
     const fileDocument = {
       type: getFileType(bucketFile.name).type,
@@ -32,52 +42,81 @@ export const uploadFile = async ({ file, ownerId, accountId, path }: UploadFileP
       bucketFileId: bucketFile.$id,
     };
 
-    const newFile = await databases.createDocument(appwriteConfig.databaseId, appwriteConfig.filesCollectionId, ID.unique(), fileDocument).catch(async (error: unknown) => {
-      await storage.deleteFile(appwriteConfig.bucketId, bucketFile.$id);
-      handleError(error, 'Failed to create file document');
-    });
+    const newFile = await databases
+      .createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.filesCollectionId,
+        ID.unique(),
+        fileDocument,
+      )
+      .catch(async (error: unknown) => {
+        await storage.deleteFile(appwriteConfig.bucketId, bucketFile.$id);
+        handleError(error, "Failed to create file document");
+      });
 
     revalidatePath(path);
     return parseStringify(newFile);
   } catch (error) {
-    handleError(error, 'Failed to upload file');
+    handleError(error, "Failed to upload file");
   }
 };
 
-const createQueries = (currentUser: Models.Document, types: string[], searchText: string, sort: string, limit?: number) => {
-  const queries = [Query.or([Query.equal('owner', [currentUser.$id]), Query.contains('users', [currentUser.email])])];
+const createQueries = (
+  currentUser: Models.Document,
+  types: string[],
+  searchText: string,
+  sort: string,
+  limit?: number,
+) => {
+  const queries = [
+    Query.or([
+      Query.equal("owner", [currentUser.$id]),
+      Query.contains("users", [currentUser.email]),
+    ]),
+  ];
 
-  if (types.length > 0) queries.push(Query.equal('type', types));
-  if (searchText) queries.push(Query.contains('name', searchText));
+  if (types.length > 0) queries.push(Query.equal("type", types));
+  if (searchText) queries.push(Query.contains("name", searchText));
   if (limit) queries.push(Query.limit(limit));
 
   if (sort) {
-    const [sortBy, orderBy] = sort.split('-');
+    const [sortBy, orderBy] = sort.split("-");
 
-    queries.push(orderBy === 'asc' ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy));
+    queries.push(
+      orderBy === "asc" ? Query.orderAsc(sortBy) : Query.orderDesc(sortBy),
+    );
   }
 
   return queries;
 };
 
-export const getFiles = async ({ types = [], searchText = '', sort = '$createdAt-desc', limit }: GetFilesProps) => {
+export const getFiles = async ({
+  types = [],
+  searchText = "",
+  sort = "$createdAt-desc",
+  limit,
+}: GetFilesProps) => {
   const { databases } = await createAdminClient();
+
   try {
     const currentUser = await getCurrentUser();
 
-    if (!currentUser) throw new Error('User not found');
+    if (!currentUser) throw new Error("User not found");
 
     const queries = createQueries(currentUser, types, searchText, sort, limit);
 
-    const files = await databases.listDocuments(appwriteConfig.databaseId, appwriteConfig.filesCollectionId, queries);
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      queries,
+    );
 
     console.log({ files });
     return parseStringify(files);
   } catch (error) {
-    handleError(error, 'Failed to get files');
+    handleError(error, "Failed to get files");
   }
 };
-
 
 export const renameFile = async ({
   fileId,
@@ -129,7 +168,6 @@ export const updateFileUsers = async ({
   }
 };
 
-
 export const deleteFile = async ({
   fileId,
   bucketFileId,
@@ -154,3 +192,45 @@ export const deleteFile = async ({
     handleError(error, "Failed to rename file");
   }
 };
+
+// ============================== TOTAL FILE SPACE USED
+export async function getTotalSpaceUsed() {
+  try {
+    const { databases } = await createSessionClient();
+    const currentUser = await getCurrentUser();
+    if (!currentUser) throw new Error("User is not authenticated.");
+
+    const files = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.filesCollectionId,
+      [Query.equal("owner", [currentUser.$id])],
+    );
+
+    const totalSpace = {
+      image: { size: 0, latestDate: "" },
+      document: { size: 0, latestDate: "" },
+      video: { size: 0, latestDate: "" },
+      audio: { size: 0, latestDate: "" },
+      other: { size: 0, latestDate: "" },
+      used: 0,
+      all: 2 * 1024 * 1024 * 1024 /* 2GB available bucket storage */,
+    };
+
+    files.documents.forEach((file) => {
+      const fileType = file.type as FileType;
+      totalSpace[fileType].size += file.size;
+      totalSpace.used += file.size;
+
+      if (
+        !totalSpace[fileType].latestDate ||
+        new Date(file.$updatedAt) > new Date(totalSpace[fileType].latestDate)
+      ) {
+        totalSpace[fileType].latestDate = file.$updatedAt;
+      }
+    });
+
+    return parseStringify(totalSpace);
+  } catch (error) {
+    handleError(error, "Error calculating total space used:, ");
+  }
+}
