e3d6e52cfb2f564252b99d49ae739a44fd71d03c
Wafiul Achdi <achdiwafiul@gmail.com>
2025-07-14 13:21:33 +0700

    feat(search): add file filtering and global search functionality

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,131 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { getFiles } from "@/lib/actions/file.actions";
+import { Models } from "node-appwrite";
+import Thumbnail from "@/components/Thumbnail";
+import FormattedDateTime from "@/components/FormattedDateTime";
+import { useDebounce } from "use-debounce";
 
 const Search = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false); 
+  const router = useRouter();
+  const path = usePathname();
+  const [debouncedQuery] = useDebounce(query, 300);
 
-export default Search
+  useEffect(() => {
+    const fetchFiles = async () => {
+      if (debouncedQuery.trim().length === 0) {
+        setResults([]);
+        setLoading(false);
+        return;
+      }
+
+      setLoading(true);
+      try {
+        const files = await getFiles({ types: [], searchText: debouncedQuery });
+        setResults(files.documents);
+      } catch (error) {
+        console.error("Failed to fetch files:", error);
+        setResults([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchFiles();
+  }, [debouncedQuery]);
+
+  useEffect(() => {
+    if (debouncedQuery.trim().length === 0) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
+  }, [debouncedQuery]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+      setResults([]);
+      setOpen(false);
+      setLoading(false);
+    }
+  }, [searchQuery]);
+
+  const handleClickItem = (file: Models.Document) => {
+    setOpen(false);
+    setResults([]);
+
+    const targetPath =
+      file.type === "video" || file.type === "audio"
+        ? "media"
+        : `${file.type}s`;
+
+    router.push(`/${targetPath}?query=${query}`);
+  };
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="Search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="Search documents, videos, and more"
+          className="search-input"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
+        {open && (
+          <ul className="search-result">
+            {loading ? (
+              <li className="loading">Loading...</li> 
+            ) : results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  className="flex items-center justify-between"
+                  key={file.$id}
+                  onClick={() => handleClickItem(file)}
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1 text-light-200"
+                  />
+                </li>
+              ))
+            ) : (
+              <li className="empty-result">No files found</li>
+            )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
